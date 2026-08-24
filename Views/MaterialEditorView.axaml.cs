0620ed842d5dd358d2b93da53b03c111f79c8757
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2026-08-24 14:20:28 +0800

    还原并修复图片拖拽功能

--- Views/MaterialEditorView.axaml.cs
+++ Views/MaterialEditorView.axaml.cs
@@ -1,9 +1,24 @@
 
+using System.Linq;
 using Avalonia.Controls;
+using Avalonia.Input;
+using VTFCreater.ViewModels;
 
 namespace VTFCreater.Views;
 
 public partial class MaterialEditorView : UserControl
 {
     public MaterialEditorView() => InitializeComponent();
+    
+    private void Slot_DragOver(object? sender, DragEventArgs e)
+    {
+        e.DragEffects = e.DataTransfer.TryGetFiles()?.Any() == true ? DragDropEffects.Copy : DragDropEffects.None;
+    }
+
+    private void Slot_Drop(object? sender, DragEventArgs e)
+    {
+        var path = e.DataTransfer.TryGetFiles()?.FirstOrDefault()?.Path.LocalPath;
+        if (path is not null && sender is Control { Tag: string key } && DataContext is MaterialEditorViewModel viewModel)
+            viewModel.AssignSlotFile(key, path);
+    }
 }
